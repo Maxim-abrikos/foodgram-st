e5c3dd0996d3a1e2944509e6b04b87e1570cde80
Maxim-abrikos <138660337+Maxim-abrikos@users.noreply.github.com>
2025-06-17 11:01:13 +0700

    Удаление api перед перезаливом

--- api/pagination.py
+++
@@ -1,5 +0,0 @@
-﻿from rest_framework.pagination import PageNumberPagination
-
-# Кастомная пагинация, позволяющая задать параметр `?limit=...`
-class CustomPagination(PageNumberPagination):
-    page_size_query_param = "limit"
--- api/permissions.py
+++
@@ -1,6 +0,0 @@
-﻿from rest_framework.permissions import BasePermission
-
-# Только автор может изменять объект, остальные могут только читать
-class IsAuthorOrReadOnly(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return request.method in ("GET",) or obj.author == request.user
